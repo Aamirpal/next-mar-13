52f6e46d6eb3a1c276344e4271e6808ca8cf8a3f
Aamirpal Sehmi <aamirpals@bridgeshealth.com>
2024-03-13 18:16:15 -0600

    update headers

--- src/app/axios-config.js
+++ src/app/axios-config.js
@@ -4,6 +4,9 @@ const axiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+        'Access-Control-Allow-Headers': 'X-CSRF-Token, Authorization',
     },
 })
 
